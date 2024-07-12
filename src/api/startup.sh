4bb274a84d9bb1868a9879d5484f4a94e9f92865
Sen Wang <senw@microsoft.com>
2024-07-12 11:56:32 -0400

    update startup

--- src/api/startup.sh
+++ src/api/startup.sh
@@ -7,6 +7,9 @@ ls -al /home/site/wwwroot/venv/bin/
 
 source /home/site/wwwroot/venv/bin/activate
 
+echo "Setting PATH..."
+export PATH="/home/site/wwwroot/venv/bin:$PATH"
+
 echo "Which Python: $(which python)"
 echo "Which Gunicorn: $(which gunicorn)"
 echo "Python Path: $(python -c 'import sys; print(sys.path)')"
